Fixed average angle calculation and distance for neighbours

basic viscek files/Vicsek_loops.py
@@ -4,19 +4,23 @@ from matplotlib.animation import FuncAnimation
 import numba
 # parameters
 L = 10.0 # size of box
-rho = 3.5 # density
+rho = 0.7 # density
 N = int(rho * L**2) # number of particles
 r0 = 1.0 # interaction radius
 deltat = 1.0 # time step
-factor = 0.5
+factor = 0.2
 v0 = r0 / deltat * factor # velocity
 iterations = 100 # animation frames
-eta = 0.05 # noise/randomness
+eta = 0.15 # noise/randomness
 max_num_neighbours = N # it could be less...
 
 # initialise positions and angles
 positions = np.random.uniform(0, L, size = (N, 2))
 angles = np.random.uniform(-np.pi, np.pi, size = N) # from 0 to 2pi rad
+
+average_angles = np.empty(iterations)
+times = np.arange(iterations)
+t=0
 
 @numba.njit
 def update(positions, angles):
@@ -34,27 +38,29 @@ def update(positions, angles):
         for j in range(N):
             distance = np.linalg.norm(positions[i] - positions[j])
             # if within interaction radius add angle to list
-            if distance < r0:
+            if (distance < r0) & (distance != 0):
                 # neighbour_angles[].append(angles[j])
                 neigh_angles[count_neigh] = angles[j]
                 count_neigh += 1
          
         # if there are neighbours, calculate average angle and noise/randomness       
         # if neighbour_angles:
+        noise = eta * np.random.uniform(-np.pi, np.pi)
         if count_neigh > 0:
-            average_angle = np.mean(neigh_angles[:count_neigh])
-            noise = eta * np.random.uniform(-np.pi, np.pi)
+            average_angle = np.angle(np.sum(np.exp((neigh_angles[:count_neigh])*1.0j)))
+            
             new_angles[i] = average_angle + noise # updated angle with noise
         else:
             # if no neighbours, keep current angle
-            new_angles[i] = angles[i]
+            new_angles[i] = angles[i] + noise
         
         # update position based on new angle
         # new position from speed and direction   
         new_positions[i] = positions[i] + v0 * np.array([np.cos(new_angles[i]), np.sin(new_angles[i])]) * deltat
         # boundary conditions of bo
         new_positions[i] %= L
-
+    # t+=1
+    # average_angles[t+1] = np.sum(new_angles)
     return new_positions, new_angles
 
 
@@ -73,8 +79,33 @@ def animate(frames):
     qv.set_offsets(positions)
     qv.set_UVC(np.cos(angles), np.sin(angles), angles)
     return qv,
- 
+
+# t=0 
+# fig, ax = plt.subplots(figsize = (12, 6), ncols=2)   
+# qv = ax[0].quiver(positions[:,0], positions[:,1], np.cos(angles), np.sin(angles), angles, clim = [-np.pi, np.pi], cmap = "hsv")
+# ax[0].set_title(f"timestep {t}")
+# ax[0].grid()
+
+
+# for i in range(1):
+#     poisitions, angles = update(positions,angles)
+#     t+=1
+# qv = ax[1].quiver(positions[:,0], positions[:,1], np.cos(angles), np.sin(angles), angles, clim = [-np.pi, np.pi], cmap = "hsv")
+# ax[1].set_title(f"timestep {t}")
+# ax[1].grid()
+
 fig, ax = plt.subplots(figsize = (6, 6))   
 qv = ax.quiver(positions[:,0], positions[:,1], np.cos(angles), np.sin(angles), angles, clim = [-np.pi, np.pi], cmap = "hsv")
+ax.set_title(f"timestep {t}")
+ax.grid()
 ani = FuncAnimation(fig, animate, frames = range(1, iterations), interval = 5, blit = True)
 plt.show()
+
+# for t,i in enumerate(times):
+#     print(t)
+#     new_positions, new_angles = update(positions, angles,t)
+#     positions = new_positions
+#     angles = new_angles
+
+# fig,ax2 = plt.subplots(figsize=(6,6))
+# ax2.plot(times,average_angles)

wall implemetation/Vicsek_loops_wall optimising.py
@@ -9,7 +9,7 @@ rho = 3 # density
 N = int(rho * L**2) # number of particles
 r0 = 0.65 # interaction radius
 deltat = 1.0 # time step
-velocity_factor = 1
+velocity_factor = 0.2
 v0 = r0 / deltat * velocity_factor # velocity
 iterations = 400 # animation frames
 eta = 0.15 # noise/randomness
@@ -22,7 +22,7 @@ wall_yMin = 1.
 wall_yMax = 9.
 wall_distance = r0 +0.8
 wall_turn = np.deg2rad(110)
-turn_factor = 10.
+turn_factor = 5.
 
 #Defining parameters for a rectangle
 x_min, x_max, y_min, y_max = 4.,6.,4.,6.
@@ -131,7 +131,7 @@ def update(positions, angles, func):
         for j in range(N):
             distance = np.linalg.norm(positions[i] - positions[j])
             # if within interaction radius add angle to list
-            if distance < r0:
+            if (distance < r0) & (distance != 0):
                 # neighbour_angles[].append(angles[j])
                 neigh_angles[count_neigh] = angles[j]
                 count_neigh += 1
@@ -142,13 +142,12 @@ def update(positions, angles, func):
         # if there are neighbours, calculate average angle and noise/randomness       
         # if neighbour_angles:
         wall_turn = varying_angle_turn(dist=distance_to_wall,turn_factor=turn_factor)
+        noise = eta * np.random.uniform(-np.pi, np.pi)
         if count_neigh > 0:
-            average_angle = np.mean(neigh_angles[:count_neigh])
-            noise = eta * np.random.uniform(-np.pi, np.pi)
-            
+            average_angle = np.angle(np.sum(np.exp((neigh_angles[:count_neigh])*1.0j)))
             
             if average_angle <= 0:
-                new_angles[i] = average_angle - wall_turn + noise
+                new_angles[i] = average_angle + wall_turn + noise
             else:
                 new_angles[i] = average_angle + wall_turn + noise
             